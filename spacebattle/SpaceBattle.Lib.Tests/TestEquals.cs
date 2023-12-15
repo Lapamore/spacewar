20769e617cbf2fda9d00d150efcac14e885a8b8d
chapa <tsikugg@mail.ru>
2023-12-15 15:58:37 +0600

    upd editconfig

--- spacebattle/SpaceBattle.Lib.Tests/TestEquals.cs
+++ spacebattle/SpaceBattle.Lib.Tests/TestEquals.cs
@@ -7,68 +7,53 @@ public class VectorTests
     [Fact]
     public void Equals_SameVector_ShouldReturnTrue()
     {
-        // Arrange
         var vector = new Vector(1, 2);
 
-        // Act
         var result = vector.Equals(vector);
 
-        // Assert
         Assert.True(result);
     }
 
     [Fact]
     public void Equals_NullObject_ShouldReturnFalse()
     {
-        // Arrange
         var vector = new Vector(1, 2);
 
-        // Act
         var result = vector.Equals(null);
 
-        // Assert
         Assert.False(result);
     }
 
     [Fact]
     public void Equals_DifferentTypeObject_ShouldReturnFalse()
     {
-        // Arrange
         var vector = new Vector(1, 2);
         var differentObject = new Mock<object>().Object;
 
-        // Act
         var result = vector.Equals(differentObject);
 
-        // Assert
         Assert.False(result);
     }
 
     [Fact]
     public void Equals_EqualVectors_ShouldReturnTrue()
     {
-        // Arrange
         var vector1 = new Vector(1, 2);
         var vector2 = new Vector(1, 2);
 
-        // Act
         var result = vector1.Equals(vector2);
 
-        // Assert
         Assert.True(result);
     }
 
     [Fact]
     public void Equals_DifferentVectors_ShouldReturnFalse()
     {
-        // Arrange
         var vector1 = new Vector(1, 2);
         var vector2 = new Vector(3, 4);
 
-        // Act
         var result = vector1.Equals(vector2);
 
-        // Assert
         Assert.False(result);
     }
 }
