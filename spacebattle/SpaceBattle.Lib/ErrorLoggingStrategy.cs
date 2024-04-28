29b0cbbdf8d7772120df3c4fc0d7a9fae79831c8
chapa <83264709+chapaflow@users.noreply.github.com>
2024-04-28 18:34:16 +0600

    unlucky))

--- spacebattle/SpaceBattle.Lib/ErrorLoggingStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpaceBattle.Lib;
-
-public class ErrorLoggingStrategy : IStrategy
-{
-    public object Invoke(object[] args)
-    {
-        return $"При выполнении команды <{((ICommand)args[0]).GetType()}> возникло исключение <{((Exception)args[1]).GetType()}>";
-    }
-}
