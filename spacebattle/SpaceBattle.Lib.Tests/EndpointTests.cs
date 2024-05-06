799d29d2c87f06a68e92879a505b011340ceaf70
chapa <83264709+chapaflow@users.noreply.github.com>
2024-05-06 08:46:40 +0600

    Lab7.1 (#13)
    * Lab7.1
    
    * Lab7.1
    
    * Lab7.1
    
    * Lab7.1
    
    * Lab7.1
    
    * Lab7.1
    
    * Lab7.1

---
+++ spacebattle/SpaceBattle.Lib.Tests/EndpointTests.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Configuration;
+using System.Net.Http.Json;
+using Hwdtech;
+using Hwdtech.Ioc;
+namespace SpaceBattle.Lib
+{
+    public class EndpointTest
+    {
+        private readonly Hashtable _idgame = new Hashtable();
+        public EndpointTest()
+        {
+            new InitScopeBasedIoCImplementationCommand().Execute();
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+            var threadDict = new ConcurrentDictionary<string, ServerThread>();
+            var senderDict = new ConcurrentDictionary<string, ISender>();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadDictionary", (object[] _) => threadDict).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SenderDictionary", (object[] _) => senderDict).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SenderGetByID", (object[] id) => senderDict[(string)id[0]]).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThreadID", (object[] id) => threadDict[(string)id[0]]).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "GetServerThreadIdByGameId", (object[] args) => { return _idgame[(int)args[0]]; }).Execute();
+
+            var startthread = new StartThread();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CreateWithStartThread", (object[] args) => startthread.Invoke(args)).Execute();
+            var hardstop = new HardStop();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "HardStop", (object[] args) => hardstop.Invoke(args)).Execute();
+            var sendcommand = new SendCommand();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SendCommand", (object[] args) => sendcommand.Invoke(args)).Execute();
+        }
+        [Fact]
+        public void IncorrectWorkTest()
+        {
+            var id = Guid.NewGuid();
+            _idgame.Add(111, id);
+            var mre = new ManualResetEvent(false);
+            var th = IoC.Resolve<ServerThread>("CreateWithStartThread", id.ToString());
+            var serv = new HttpClient(new HttpClientHandler());
+            serv.BaseAddress = new Uri("http://localhost:12233");
+            var prms = new Dictionary<string, object>();
+            prms.Add("can rotate", true);
+            prms.Add("rotation speed", 4);
+            var mess = new Mess("WRONG!", 112, prms);
+            var request = System.Net.HttpStatusCode.BadRequest;
+            var command = new Mock<ICommand>();
+            command.Setup(x => x.Execute()).Verifiable();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CheckCommandWork", (object[] args) => { return command.Object; }).Execute();
+            var ns = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Current"));
+            Endpoint.Run(ns);
+            var rec = JsonContent.Create(mess);
+            var ans = serv.PostAsync("/message", rec);
+            Assert.Equal(request, ans.Result.StatusCode);
+            Endpoint.Stop();
+            var hardstop = IoC.Resolve<ICommand>("HardStop", id.ToString(), () => { mre.Set(); });
+            var sender = IoC.Resolve<ISender>("SenderGetByID", id.ToString());
+            var send = IoC.Resolve<ICommand>("SendCommand", sender, hardstop);
+            send.Execute();
+            mre.WaitOne();
+            th.Wait();
+            command.Verify(x => x.Execute(), Times.Never());
+        }
+        [Fact]
+        public void CorrectWorkTest()
+        {
+            var id = Guid.NewGuid();
+            _idgame.Add(111, id);
+            var mre = new ManualResetEvent(false);
+            var th = IoC.Resolve<ServerThread>("CreateWithStartThread", id.ToString());
+            var server = new HttpClient(new HttpClientHandler());
+            server.BaseAddress = new Uri("http://localhost:12233");
+            var prms = new Dictionary<string, object>();
+            prms.Add("can rotate", true);
+            prms.Add("rotation speed", 4);
+            var mess = new Mess("StartServer", 111, prms);
+            var request = System.Net.HttpStatusCode.OK;
+            var command = new Mock<ICommand>();
+            command.Setup(x => x.Execute()).Verifiable();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CheckCommandWork", (object[] args) => { return command.Object; }).Execute();
+            var ns = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Current"));
+            Endpoint.Run(ns);
+            var rec = JsonContent.Create(mess);
+            var ans = server.PostAsync("/message", rec);
+            Assert.Equal(request, ans.Result.StatusCode);
+            Endpoint.Stop();
+            var hardstop = IoC.Resolve<ICommand>("HardStop", id.ToString(), () => { mre.Set(); });
+            var sender = IoC.Resolve<ISender>("SenderGetByID", id.ToString());
+            var send = IoC.Resolve<ICommand>("SendCommand", sender, hardstop);
+            send.Execute();
+            mre.WaitOne();
+            th.Wait();
+            command.Verify(x => x.Execute(), Times.Once());
+        }
+    }
+}
---
+++ spacebattle/SpaceBattle.Lib/Message.cs
@@ -0,0 +1,4 @@
+namespace SpaceBattle.Lib
+{
+    public record Mess(string cmd, int id, Dictionary<string, object> prms);
+}
