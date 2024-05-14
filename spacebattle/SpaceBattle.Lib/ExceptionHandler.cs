6b5aad1f5b9f6dcb735a33b51ff3e465a8d45db8
chapa <tsikugg@mail.ru>
2024-05-14 19:40:57 +0600

    smth lab8.1

---
+++ spacebattle/SpaceBattle.Lib/ExceptionHandler.cs
@@ -0,0 +1,13 @@
+namespace SpaceBattle.Lib
+{
+    public class ExceptionHandler : IStrategy
+    {
+        public object Invoke (params object[] args)
+        {
+            ICommand cmd = (ICommand) args[0];
+            Exception exc = (Exception) args[1];
+    
+            throw exc;
+        }
+    }
+}
---
+++ spacebattle/SpaceBattle.Lib/GameCommand.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using Hwdtech;
+
+namespace SpaceBattle.Lib
+{
+    public class GameCommand : ICommand
+    {
+        object _scope;
+        IReciever _rec;
+        Stopwatch stopwatch = new Stopwatch();
+        public GameCommand(object scope, IReciever rec)
+        {
+            _scope = scope;
+            _rec = rec;
+        }
+
+        public void Execute()
+        {
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", _scope).Execute();
+            var gametime = IoC.Resolve<int>("GameQuantum");
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            while (stopwatch.ElapsedMilliseconds <= gametime && !_rec.IsEmpty())
+            {
+                var cmd = _rec.Recieve();
+
+                try 
+                {
+                    cmd.Execute();
+                }
+
+                catch (Exception ex)
+                {
+                    IoC.Resolve<ICommand>("ExceptionHandlerGame", cmd, ex).Execute();
+                }
+            }
+
+            stopwatch.Stop();  
+        }
+    }
+}
